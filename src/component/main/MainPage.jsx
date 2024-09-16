0dc79e4fc93a5735ad90eaa1f668dc43bb1fd2ce
ifham <mail2ifham@gmail.com>
2024-09-16 13:42:56 +0530

    bug fixed

--- src/component/main/MainPage.jsx
+++ src/component/main/MainPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import FormAge from "../fromAge/FormAge";
 import DisplayAge from "../displayAge/DisplayAge";
-import "./MainPage.css";
+import "./mainPage.css";
 import useAge from "./useAge";
 
 function MainPage() {
